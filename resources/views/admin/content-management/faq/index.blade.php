63118869905787b0dc7dddfc6e3407c1eef90cdb
jocen <silveriusjonathancendana@gmail.com>
2024-12-02 12:03:17 +0700

    redesign table

--- resources/views/admin/content-management/faq/index.blade.php
+++ resources/views/admin/content-management/faq/index.blade.php
@@ -3,19 +3,37 @@
 @section('content')
 <div class="pad-content">
     <div class="container">
-        <div class="row">
-            <div class="col-md-5 my-auto">
-                <div class="title mb0">FAQ</div>
-            </div>
-            <div class="col-md-7 my-auto">
-                <ul class="l-btn right">
-                    <li>
-                        <a href="{{ URL::to('/admin/content-management/faq/add') }}">
-                            <button class="hvr-button" type="button">Add FAQ</button>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+        <div class="title">FAQ</div>
+        <div class="css-table">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>FAQ Content</th>
+                        <th width="30%">Last Updated On</th>
+                        <th width="20%">Action(s)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>General FAQ</td>
+                        <td>01 Nov 2024, 11:20AM</td>
+                        <td>
+                            <div class="link-global">
+                                <a href="{{ URL::to('/admin/content-management/faq/edit') }}">Edit details</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Vendor FAQ</td>
+                        <td>01 Nov 2024, 11:20AM</td>
+                        <td>
+                            <div class="link-global">
+                                <a href="{{ URL::to('/admin/content-management/faq/edit') }}">Edit details</a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
@@ -25,6 +43,14 @@
 <script type="text/javascript">
     $(document).ready(function () {
         $('.nav-group-faq a').addClass('active');
+
+        var table = $('.table').DataTable({
+            ordering: false,
+            lengthChange: false,
+            searching: false,
+            info: false,
+            paging: false,
+        });
     });
 </script>
 @endsection
--- resources/views/admin/content-management/pet-cost-calculator/index.blade.php
+++ resources/views/admin/content-management/pet-cost-calculator/index.blade.php
@@ -4,6 +4,37 @@
 <div class="pad-content">
     <div class="container">
         <div class="title">Pet Cost Calculator</div>
+        <div class="css-table">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Pet Type</th>
+                        <th width="30%">Last Updated On</th>
+                        <th width="20%">Action(s)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Dog</td>
+                        <td>01 Nov 2024, 11:20AM</td>
+                        <td>
+                            <div class="link-global">
+                                <a href="{{ URL::to('/admin/content-management/pet-cost-calculator/edit') }}">Edit details</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Cat</td>
+                        <td>01 Nov 2024, 11:20AM</td>
+                        <td>
+                            <div class="link-global">
+                                <a href="{{ URL::to('/admin/content-management/pet-cost-calculator/edit') }}">Edit details</a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
@@ -12,6 +43,14 @@
 <script type="text/javascript">
     $(document).ready(function () {
         $('.nav-group-pet-cost a').addClass('active');
+
+        var table = $('.table').DataTable({
+            ordering: false,
+            lengthChange: false,
+            searching: false,
+            info: false,
+            paging: false,
+        });
     });
 </script>
 @endsection
